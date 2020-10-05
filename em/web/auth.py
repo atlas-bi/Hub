d20885216e35ed2e4566d98b4589955ac8d78be5
Christopher Pickering <cpickering@rhc.net>
2020-10-05 10:48:25 -0700

    updated name and id of demo user to match db

--- em/web/auth.py
+++ em/web/auth.py
@@ -31,8 +31,8 @@ def before_request():
     """ checks and reloads auth before each request """
     #if "user_id" in session:
     g.user = []  # ldap.get_object_details(user=session["user_id"], dn_only=False)
-    g.user_id = "1"  # g.user["employeeID"][0].decode("utf-8")
-    g.user_full_name = "Default"  # g.user["name"][0].decode("utf-8")
+    g.user_id = "1234"  # g.user["employeeID"][0].decode("utf-8")
+    g.user_full_name = "Boss Man"  # g.user["name"][0].decode("utf-8")
     g.ldap_groups = []  # ldap.get_user_groups(user=session["user_id"])
 
 
