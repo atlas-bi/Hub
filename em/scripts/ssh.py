c95f1907116979edcdbce7cd3a4e51a62448561a
Christopher Pickering <cpickering@rhc.net>
2020-10-19 09:47:18 -0700

    modified ssh script to capture errors

--- em/scripts/ssh.py
+++ em/scripts/ssh.py
@@ -58,32 +58,43 @@ class Ssh:
             # there is no timeout in paramiko so...
             # continue to attemp to login during time limit
             # if we are getting timeout exceptions
-            timeout = time.time() + 60 * 3  #  2 mins from now
-            while True:
-                try:
-                    self.transport = paramiko.Transport(
-                        self.connection.address
-                        + ":"
-                        + str((self.connection.port or 22))
-                    )
-                    self.transport.connect(
-                        username=self.connection.username,
-                        password=em_decrypt(self.connection.password),
-                    )
 
-                    self.session = self.transport.open_channel(kind="session")
-                    break
-                except paramiko.ssh_exception.AuthenticationException as e:
-                    # pylint: disable=no-else-continue
-                    if str(e) == "Authentication timeout." and time.time() <= timeout:
-                        time.sleep(10)  # wait 10 sec before retrying
-                        continue
-                    elif time.time() > timeout:
-                        # pylint: disable=raise-missing-from
-                        raise ValueError("SFTP Connection timeout.")
+            self.session = paramiko.SSHClient()
+            self.session.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+            self.session.connect(
+                hostname=self.connection.address,
+                port=(self.connection.port or 22),
+                username=self.connection.username,
+                password=em_decrypt(self.connection.password),
+                timeout=5000,
+            )
 
-                    else:
-                        raise e
+            # timeout = time.time() + 60 * 3  #  2 mins from now
+            # while True:
+            #     try:
+            #         self.transport = paramiko.Transport(
+            #             self.connection.address
+            #             + ":"
+            #             + str((self.connection.port or 22))
+            #         )
+            #         self.transport.connect(
+            #             username=self.connection.username,
+            #             password=em_decrypt(self.connection.password),
+            #         )
+
+            #         self.session = self.transport.open_channel(kind="session")
+            #         break
+            #     except paramiko.ssh_exception.AuthenticationException as e:
+            #         # pylint: disable=no-else-continue
+            #         if str(e) == "Authentication timeout." and time.time() <= timeout:
+            #             time.sleep(10)  # wait 10 sec before retrying
+            #             continue
+            #         elif time.time() > timeout:
+            #             # pylint: disable=raise-missing-from
+            #             raise ValueError("SFTP Connection timeout.")
+
+            #         else:
+            #             raise e
         # pylint: disable=bare-except
         except:
             logging.error(
@@ -121,7 +132,7 @@ class Ssh:
                 str(self.task.last_run_job_id),
             )
             self.session.close()
-            self.transport.close()
+            # self.transport.close()
 
         # pylint: disable=bare-except
         except:
@@ -149,32 +160,47 @@ class Ssh:
         self.__connect()
 
         try:
-            self.session.exec_command(self.command)
+            stdin, stdout, stderr = self.session.exec_command(
+                self.command, timeout=5000
+            )
 
-            while self.session.recv_ready():
-                self.stdout_data.append(self.session.recv(100).decode("utf-8"))
+            self.stderr_data = b""
+            self.stdout_data = b""
 
-            self.stdout_data = "".join(self.stdout_data)
+            while not stdout.channel.exit_status_ready():
+                self.stdout_data += stdout.channel.recv(1024)
 
-            while self.session.recv_stderr_ready():
-                self.stderr_data.append(self.session.recv_stderr(100).decode("utf-8"))
+            for line in iter(stdout.readline, ""):
+                self.stdout_data += bytes(line,'utf8')
 
-            self.stderr_data = "".join(self.stderr_data)
+            for line in iter(stderr.readline, ""):
+                self.stderr_data += bytes(line,'utf8')
 
-            if self.stderr_data != "" or self.session.recv_exit_status() != 1:
+
+            if self.stderr_data:
+                print("error", str(self.stderr_data, "utf8"))
+
+            if self.stdout_data:
+                print("success", str(self.stdout_data, "utf8"))
+
+            if stdout.channel.recv_exit_status() != 0:
 
                 logging.error(
-                    "SSH: Error output: Task: %s, with run: %s\n%s",
+                    "SSH: Error output: Task: %s, with run: %s\n%s\n%s",
                     str(self.task.id),
                     str(self.task.last_run_job_id),
-                    str(self.stderr_data),
+                    str(self.stdout_data,"utf8"),
+                    str(self.stderr_data,"utf8")
                 )
                 log = TaskLog(
                     task_id=self.task.id,
                     job_id=self.task.last_run_job_id,
                     status_id=19,  # ssh
                     error=1,
-                    message="SSH Error:\n" + str(self.stderr_data),
+                    message="SSH Error:\n"
+                    + str(self.stdout_data, "utf8")
+                    + "\n"
+                    + str(self.stderr_data, "utf8"),
                 )
                 db.session.add(log)
                 db.session.commit()
@@ -182,16 +208,20 @@ class Ssh:
             if self.stdout_data != "":
 
                 logging.info(
-                    "SSH: Output: Task: %s, with run: %s\n%s",
+                    "SSH: Output: Task: %s, with run: %s\n%s\n%s",
                     str(self.task.id),
                     str(self.task.last_run_job_id),
-                    str(self.stdout_data),
+                    str(self.stdout_data,"utf8"),
+                    str(self.stderr_data,"utf8")
                 )
                 log = TaskLog(
                     task_id=self.task.id,
                     job_id=self.task.last_run_job_id,
                     status_id=19,  # ssh
-                    message="SSH Output:\n" + str(self.stdout_data),
+                    message="SSH Output:\n"
+                    + str(self.stdout_data, "utf8")
+                    + "\n"
+                    + str(self.stderr_data, "utf8"),                   
                 )
                 db.session.add(log)
                 db.session.commit()
@@ -209,7 +239,10 @@ class Ssh:
                 job_id=self.task.last_run_job_id,
                 status_id=19,  # ssh
                 error=1,
-                message="SSH Error:\n" + str(full_stack()),
+                message="SSH Error:\n"
+                + (str(self.stderr_data, "utf8") + "\n" if self.stderr_data else "")
+                + (str(self.stdout_data, "utf8") + "\n" if self.stdout_data else "")
+                + str(full_stack()),
             )
             db.session.add(log)
             db.session.commit()
