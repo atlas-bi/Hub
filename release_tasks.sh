93eee77dc7e7fee0c38703572625242f5b50c096
Christopher Pickering <christopher.pickering@keemail.me>
2021-05-25 14:42:27 -0500

    Update release_tasks.sh

--- release_tasks.sh
+++ release_tasks.sh
@@ -3,10 +3,6 @@ export FLASK_ENV=development
 export FLASK_DEBUG=True
 export FLASK_APP=em_web
 
-psql --command "CREATE USER webapp WITH SUPERUSER PASSWORD 'nothing';"
-dropdb em_web_dev
-createdb -O webapp em_web_dev
-
 flask db init
 flask db migrate
 flask db upgrade
