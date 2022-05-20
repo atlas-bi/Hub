6a227dea56a44ae7c7b1f9edcab01b43fe92cdd7
Christopher Pickering <christopher.pickering@keemail.me>
2022-05-20 13:46:38 -0500

    fixed test for params

--- runner/tests/test_scripts_params.py
+++ runner/tests/test_scripts_params.py
@@ -6,7 +6,7 @@ run with::
        --cov --cov-append --cov-branch --cov-report=term-missing --disable-warnings
 
 
-   poetry run pytest runner/tests/test_scripts_params.py::test_next \
+   poetry run pytest runner/tests/test_scripts_params.py::test_date_parsing \
        --cov --cov-append --cov-branch  --cov-report=term-missing --disable-warnings
 
 """
@@ -14,7 +14,7 @@ run with::
 from pytest import fixture
 
 from runner.extensions import db
-from runner.model import Task
+from runner.model import ProjectParam, Task, TaskParam
 from runner.scripts.em_params import ParamLoader
 
 from .conftest import create_demo_task
@@ -29,13 +29,13 @@ def test_date_parsing(client_fixture: fixture) -> None:
     task.source_query_type_id = 4
     task.source_query = "declare @stuff nvarchar(max) = 'asdf'"
 
-    db.session.add(TaskParam(task_id=task.id, value="@stuff", key="1234"))
+    db.session.add(TaskParam(task_id=task.id, key="@stuff", value="1234"))
     db.session.commit()
 
     assert ParamLoader(task, None).project_params == {}
     assert ParamLoader(task, None).task_params == {"@stuff": "1234"}
 
-    db.session.add(TaskParam(project_id=task.project.id, value="@stuff", key="2345"))
+    db.session.add(ProjectParam(project_id=task.project.id, key="@stuff", value="2345"))
     db.session.commit()
 
     assert ParamLoader(task, None).project_params == {"@stuff": "2345"}
