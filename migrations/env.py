739c0641e51ebe7e0565e4019f6015ab6fc1353a
Christopher Pickering <christopher@going.bg>
2023-06-13 11:47:31 -0500

    fix(db): fixed issue with db migrations on latest flask-migrate+sqlalchemy

--- migrations/env.py
+++ migrations/env.py
@@ -4,7 +4,6 @@ import logging
 from logging.config import fileConfig
 
 from alembic import context
-from sqlalchemy import engine_from_config, pool
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -21,17 +20,34 @@ logger = logging.getLogger("alembic.env")
 # target_metadata = mymodel.Base.metadata
 from flask import current_app
 
-config.set_main_option(
-    "sqlalchemy.url",
-    str(current_app.extensions["migrate"].db.engine.url).replace("%", "%%"),
-)
-target_metadata = current_app.extensions["migrate"].db.metadata
+def get_engine():
+     try:
+         # this works with Flask-SQLAlchemy<3 and Alchemical
+         return current_app.extensions['migrate'].db.get_engine()
+     except TypeError:
+         # this works with Flask-SQLAlchemy>=3
+         return current_app.extensions['migrate'].db.engine
+
+
+ def get_engine_url():
+     try:
+         return get_engine().url.render_as_string(hide_password=False).replace(
+             '%', '%%')
+     except AttributeError:
+         return str(get_engine().url).replace('%', '%%')
+
+config.set_main_option('sqlalchemy.url', get_engine_url())
+target_db = current_app.extensions['migrate'].db
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
+def get_metadata():
+    if hasattr(target_db, 'metadatas'):
+        return target_db.metadatas[None]
+    return target_db.metadata
 
 def run_migrations_offline():
     """Run migrations in 'offline' mode.
@@ -46,7 +62,7 @@ def run_migrations_offline():
 
     """
     url = config.get_main_option("sqlalchemy.url")
-    context.configure(url=url, target_metadata=target_metadata, literal_binds=True)
+    context.configure(url=url, target_metadata=get_metadata(), literal_binds=True)
 
     with context.begin_transaction():
         context.run_migrations()
@@ -70,16 +86,12 @@ def run_migrations_online():
                 directives[:] = []
                 logger.info("No changes in schema detected.")
 
-    connectable = engine_from_config(
-        config.get_section(config.config_ini_section),
-        prefix="sqlalchemy.",
-        poolclass=pool.NullPool,
-    )
+    connectable = get_engine()
 
     with connectable.connect() as connection:
         context.configure(
             connection=connection,
-            target_metadata=target_metadata,
+            target_metadata=get_metadata(),
             process_revision_directives=process_revision_directives,
             **current_app.extensions["migrate"].configure_args
         )
